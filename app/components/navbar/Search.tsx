7cf6ff14c4982ad73392c3330186bc0db4e4a960
阿振ccc <614317979@qq.com>
2023-05-11 13:25:26 +0800

    initial project

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+const Search = () => {
+  return (
+    <div 
+      className="
+        border-[1px] 
+        w-full 
+        md:w-auto 
+        py-2
+        rounded-full
+        shadow-sm
+        hover:shadow-md
+        transition
+        cursor-pointer
+      "
+    >
+      Search
+    </div>
+  )
+}
+
+export default Search
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+
+const font = Nunito({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
